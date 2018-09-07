d47de28b0ba309295626c1edd3ead3b22b532f05
Rodrigo Campos <rodrigo@upemedia.com>
2018-09-06 20:03:18 -0600

    Edits Jobs Single

--- plugins/jobs-plus/app/views/job-archive/main.php
+++ plugins/jobs-plus/app/views/job-archive/main.php
@@ -1,106 +1,128 @@
-<div class="ig-container">
-    <div class="hn-container">
-        <div class="job-search">
-            <form method="get"
-                  action="<?php echo is_singular() ? get_permalink(get_the_ID()) : get_post_type_archive_link('jbp_job') ?>">
-                <div class="search input-group input-group-lg has-feedback" role="search" id="mySearch">
-                    <input style="border-radius: 0;box-sizing: border-box" name="query" value="<?php echo $search ?>"
-                           type="search"
-                           class="form-control job-query"
-                           placeholder="<?php echo __('Search For Job', je()->domain) ?>"/>
-<span class="input-group-btn">
-    <button style="border-radius: 0" class="btn btn-default" type="submit">
-        <span class="glyphicon glyphicon-search"></span>
-        <span class="sr-only">Search</span>
-    </button>
-  </span>
-                </div>
-                <?php do_action('jbp_job_listing_after_search_form') ?>
-            </form>
-        </div>
-        <?php if (empty($chunks)): ?>
-            <h2><?php _e('No Job Found', je()->domain); ?></h2>
-        <?php else: ?>
-            <div class="jbp-job-list">
-                <?php foreach ($chunks as $chunk): ?>
-                    <div class="row no-margin">
-                        <?php foreach ($chunk as $key => $col): ?>
-                            <?php
-                            $color = '';
-                            if ($colors) {
-                                $color = $colors[array_rand($colors)];
-                            }
-                            $model = $col['item'];
-                            $size = $col['class'];
+<div class="ig-container pag-chance">
+	<div class="hn-container">
+		<div class="jbp-job-single">
+			<?php
+			if ( isset( $model->job_img ) && $model->job_img != '' && is_numeric( $model->job_img ) ) {
+				$image = wp_get_attachment_url( $model->job_img );
+				?>
+				<div class="row">
+					<div style="margin-bottom: 20px;">
+						<img src="<?php echo $image ?>">
+					</div>
+				</div>
+			<?php } ?>
+			<div class="row jobs-meta">
+				<div class="col-md-3 jobs-meta-row">
+                    <i class="fa fa-money" aria-hidden="true"></i>
+					<h5><?php _e( 'Job Budget', je()->domain ); ?></h5>
+					<small class="text-warning"><?php $model->render_prices() ?></small>
+				</div>
+				<div class="col-md-3 jobs-meta-row">
+                    <i class="fa fa-clock-o" aria-hidden="true"></i>
+                    <h5><?php _e( 'This job open for', je()->domain ) ?></h5>
+					<small class="text-warning"><?php echo $model->get_due_day() ?></small>
+				</div>
+				<div class="col-md-3 jobs-meta-row">
+					<i class="fa fa-calendar" aria-hidden="true"></i>
+					<h5><?php _e( 'Must be completed by', je()->domain ) ?></h5>
+					<?php if ( strtotime( $model->dead_line ) ): ?>
+						<small
+							class="text-warning"><?php echo date_i18n( get_option( 'date_format' ), strtotime( $model->dead_line ) ); ?></small>
+					<?php else: ?>
+						<small class="text-warning"><?php _e( 'N/A', je()->domain ) ?></small>
+					<?php endif; ?>
+				</div>
 
-                            setup_postdata($model->wp_post);
-                            ?>
-                            <div <?php echo $key == 0 ? 'style="margin-left:0"' : null ?>
-                                class="jbp_job_item <?php echo $size; ?> no-padding">
-                                <div class="jbp_job_except <?php echo $color ?>">
-                                    <div class="jbp_inside">
-                                        <div class="row">
-                                            <?php if( isset( $model->job_img ) && $model->job_img != '' ) { ?>
-                                            <div style="padding-right: 0" class="col-sm-3">
-                                                <?php $image = wp_get_attachment_url( $model->job_img ); ?>
-                                                <img src="<?php echo $image ?>" alt="<?php echo wp_trim_words($model->job_title, 10) ?>">
-                                            </div>
-                                            <?php } ?>
-                                            <div class="col-sm-<?php echo isset( $model->job_img ) && $model->job_img != '' ? 9 : 12; ?>">
-                                                <h4>
-                                                    <a href="<?php echo get_permalink($model->id) ?>"><?php echo wp_trim_words($model->job_title, 10) ?></a>
-                                                </h4>
-                                                <?php if ($model->is_expired()): ?>
-                                                    <?php _e( "This job is expired", je()->domain ) ?>
-                                                <?php else: ?>
-                                                    <?php if (!isset($lite) || $lite == false): ?>
-                                                        <div class="ellipsis">
-                                                            <?php
-                                                            $content = get_the_content();
-                                                            //cal the except words count base on element width
-                                                            $sub = count($chunk);
-                                                            if (isset($col['text_length'])) {
-                                                                $sub = $col['text_length'];
-                                                            }
-                                                            $content = strip_tags($content);
-                                                            $charlength = 36 / $sub;
-                                                            echo apply_filters('jbp_job_list_content', wp_trim_words($content, $charlength), $content, $charlength);
-                                                            ?>
-                                                        </div>
-                                                    <?php endif; ?>
-                                                <?php endif; ?>
-                                                </div>
-                                            <div style="clear: both"></div>
-                                        </div>
-                                        <div class="jbp_job_bottom">
-                                            <div class="jbp_terms">
-                                                <?php echo the_terms($model->id, 'jbp_category', __('Categories: ', je()->domain), ', ', ' '); ?>
-                                            </div>
-                                            <div class="jbp_meta">
-                                                <div class="pull-left">
-                                                    <?php _e('Due: ', je()->domain); ?><?php echo $model->get_end_date() ?>
-                                                </div>
-                                                <div class="pull-right">
-                                                    <?php _e('Budget: ', je()->domain); ?>
-                                                    <?php echo
-                                                    $model->render_prices('max');
-                                                    ?>
-                                                </div>
-                                            </div>
-                                        </div>
+				<div class="col-md-3 jobs-meta-row">
+					<?php if ( strtolower( $model->get_due_day() ) != 'expired' && je()->settings()->job_contact_form == 0 ): ?>
+						<?php if ( JobsExperts_Helper::is_user_pro( get_current_user_id() ) ): ?>
+							<?php ob_start(); ?>
+							<a class="btn btn-info btn-sm jbp_contact_job"
+							   href="<?php echo esc_url( add_query_arg( array(
+								   'contact' => get_post()->post_name
+							   ), apply_filters( 'jbp_job_contact_link', get_permalink( je()->pages->page( JE_Page_Factory::JOB_CONTACT ) ), get_the_ID() ) ) ) ?>"><?php _e( 'Contact', je()->domain ) ?></a>
+							<?php $content = ob_get_clean();
+							echo apply_filters( 'jbp_job_contact_btn', $content, $model );
+							?>
+						<?php else: ?>
+							<a class="btn btn-info btn-sm"
+							   href="<?php echo get_permalink( je()->pages->page( JE_Page_Factory::EXPERT_ADD ) ) ?>"><?php _e( 'Become Expert', je()->domain ) ?></a>
+						<?php endif; ?>
+					<?php else: ?>
+						<a disabled class="btn btn-info btn-sm"
+						   href="#"><?php _e( 'Contact', je()->domain ) ?></a>
+					<?php endif; ?>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="row job-content">
+				<div class="col-md-12">
+					<?php echo wpautop( JobsExperts_Helper::jbp_html_beautifier( jbp_filter_text( $model->description ) ) ) ?>
+				</div>
+				<div class="col-md-12">
+					<?php
+					$skills = $model->skills;
+					if ( ! empty( $skills ) ): ?>
+						<div class="job_skills">
+							<?php
+							echo get_the_term_list( $model->id, 'jbp_skills_tag', __( '<h4>You will need to have these skills:', je()->domain ) . '</h4><ul><li>', '</li><li>', '</li></ul>' )
+							?>
+						</div>
+					<?php endif; ?>
+				</div>
+			</div>
+			<?php do_action( 'je_job_single_before_attachments', $model ) ?>
+			<div class="row">
+				<div class="col-md-12">
+					<?php
+					$files = array_unique( array_filter( explode( ',', $model->portfolios ) ) );
+					if ( ! empty( $files ) ): ?>
+						<div class="row-fluid full">
+							<div class="page-header">
+								<label><?php _e( 'Sample Files', je()->domain ) ?></label>
+							</div>
+							<?php
+							ig_uploader()->show_media( $model, 'portfolios' );
+							?>
+							<div class="clearfix"></div>
+						</div>
+					<?php endif; ?>
+				</div>
+			</div>
 
-                                    </div>
-                                </div>
-                            </div>
-                        <?php endforeach; ?>
-                        <div style="clear: both"></div>
-                    </div>
-                <?php endforeach; ?>
-            </div>
-            <?php $this->render_partial('job-archive/_paging', array(
-                'total_pages' => $total_pages
-            )) ?>
-        <?php endif; ?>
-        <div style="clear: both"></div>
-    </div>
+			<div class="row">
+				<div class="col-md-12">
+					<?php if ( $model->is_current_owner() ): ?>
+						<br/>
+					<?php $post = get_post( $model->id );
+					$var        = $post->post_status == 'publish' ? $post->post_name : $post->ID;
+					?>
+						<a class="btn btn-primary"
+						   href="<?php echo esc_url( add_query_arg( array( 'job' => $var ), apply_filters( 'job_edit_button_link', get_permalink( je()->pages->page( JE_Page_Factory::JOB_EDIT ) ) ) ) ) ?>">
+							<?php _e( 'Edit', je()->domain ) ?>
+						</a>
+						<form class="frm-delete" method="post" style="display: inline-block">
+							<input name="job_id" type="hidden" value="<?php echo $model->id ?>">
+							<?php wp_nonce_field( 'delete_job_' . $model->id ) ?>
+							<button name="delete_job" class="btn btn-danger"
+							        type="submit"><?php _e( 'Trash', je()->domain ) ?></button>
+						</form>
+						<script type="text/javascript">
+							jQuery(document).ready(function ($) {
+								$('.frm-delete').submit(function () {
+									if (confirm('<?php echo esc_js( __( 'Are you sure?', je()->domain ) ) ?>')) {
+
+									} else {
+										return false;
+									}
+								})
+							})
+						</script>
+					<?php endif; ?>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
 </div>
